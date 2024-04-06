cbe8742cf6f26addec3e7f00236a99c0d60731b8
talha khan <160160121+talha2597@users.noreply.github.com>
2024-04-06 10:25:38 +0530

    Initial commit

---
+++ ethereum.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://eth1.lava.build/lava-referer-5b80983e-8352-4712-8958-52b47ac31ed1/"
+
+OUTPUT_DIR="logs"
+
+eth_data=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$eth_data" >> "$OUTPUT_DIR/ethereum.txt"
+    
+    echo "Ethereum data fetched and saved to $OUTPUT_DIR/ethereum.txt."
+else
+    echo "Failed to fetch Ethereum data."
+fi
